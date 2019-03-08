b8557045514ac78438b2d9d6d81dbf32d34143e5
Edinei J. Bauer <galera.org@gmail.com>
2019-03-08 17:23:21 -0300

    sessao data

--- public/set/sessao.php
+++ public/set/sessao.php
@@ -8,10 +8,9 @@ if (isset($_SESSION['userlogin']) && !empty($_SESSION['userlogin']['token'])) {
     $read = new Read();
     $prazoTokenExpira = date('Y-m-d H:i:s', strtotime("-2 months", strtotime(date("Y-m-d H:i:s"))));
     $read->exeRead("usuarios", "WHERE token = :to", "to={$_SESSION['userlogin']['token']}");
-
     if ($read->getResult() && $read->getResult()[0]['status'] === "1" && $read->getResult()[0]['token_expira'] > $prazoTokenExpira) {
-        $data['data'] = $read->getResult()[0];
-        $data['data']['imagem'] = !empty($data['data']['imagem']) && $data['data']['imagem'] !== "[]" && \Helpers\Check::isJson($data['data']['imagem']) ? json_decode($data['data']['imagem'], true)[0]['image'] : "";
+        $data['data'] = $_SESSION['userlogin'];
+        $data['data']['setor'] = $data['data']['setor']['entity'];
     } else {
         $login->logout();
     }

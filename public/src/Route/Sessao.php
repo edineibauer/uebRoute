7de7ef170f0e86bf8ea42d48958f20a161b43370
Edinei J. Bauer <galera.org@gmail.com>
2019-08-02 22:25:18 -0300

    sessao error

--- public/src/Route/Sessao.php
+++ public/src/Route/Sessao.php
@@ -33,7 +33,7 @@ class Sessao
         $prazoTokenExpira = date('Y-m-d H:i:s', strtotime("-2 months", strtotime(date("Y-m-d H:i:s"))));
         $sql = new \Conn\SqlCommand();
         $sql->exeCommand("SELECT u.* FROM " . PRE . "usuarios as u JOIN " . PRE . "usuarios_token as t ON u.id = t.usuario WHERE t.token = '" . $_COOKIE['token'] . "' AND u.status = 1 AND t.token_expira > " . $prazoTokenExpira);
-        if($sql->getResult()) {
+        if($sql->getResult() && !empty($sql->getResult()[0]['nome'])) {
             new Login(["user" => $sql->getResult()[0]['nome'], "password" => $sql->getResult()[0]['password']], !1);
         } else {
             setcookie("token", '', -1);

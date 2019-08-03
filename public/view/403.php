494bd812ea9b30af72a9f36aa79ec89b09163f65
Edinei J. Bauer <galera.org@gmail.com>
2019-08-03 15:06:06 -0300

    send to login in 403 pages

--- public/view/403.php
+++ public/view/403.php
@@ -5,17 +5,17 @@ ob_start();
     <div class="panel align-center" style="max-width: 900px; margin: auto; float: initial">
 
         <div class="row">
-            <div class="col s12 m6">
+            <div class="col s12">
                 <br>
                 <div class="panel font-xlarge font-light padding-32">
-                    Acesso Negado
+                    acesso não permitido
                 </div>
             </div>
         </div>
 
         <br><br>
         <div class="align-center">
-            <a class="btn-large opacity hover-shadow color-white" style="text-decoration: none; margin: auto; float: initial" href="<?= HOME ?>login">Fazer Login</a>
+            <a class="btn-large opacity hover-opacity-off radius hover-shadow theme" style="text-decoration: none; margin: auto; float: initial" href="<?= HOME ?>login">Fazer Login</a>
         </div>
 
     </div>

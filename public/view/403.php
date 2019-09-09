d36b0b850c3020bc62b51b51c078cd08d9b45878
Edinei J. Bauer <galera.org@gmail.com>
2019-09-09 10:02:26 -0300

    fix css

--- public/view/403.php
+++ public/view/403.php
@@ -1,25 +1,29 @@
 <?php
 ob_start();
 ?>
-<div class="row">
-    <div class="panel align-center" style="max-width: 900px; margin: auto; float: initial">
-
+    <div class="r-403">
         <div class="row">
-            <div class="col s12">
-                <br>
-                <div class="panel font-xlarge font-light padding-32">
-                    acesso não permitido
+            <div class="panel align-center" style="max-width: 900px; margin: auto; float: initial">
+
+                <div class="row">
+                    <div class="col s12">
+                        <br>
+                        <div class="panel font-xlarge font-light padding-32">
+                            acesso não permitido
+                        </div>
+                    </div>
                 </div>
+
+                <br><br>
+                <div class="align-center">
+                    <a class="btn-large opacity hover-opacity-off radius hover-shadow theme"
+                       style="text-decoration: none; margin: auto; float: initial" href="<?= HOME ?>login">Fazer
+                        Login</a>
+                </div>
+
             </div>
         </div>
-
-        <br><br>
-        <div class="align-center">
-            <a class="btn-large opacity hover-opacity-off radius hover-shadow theme" style="text-decoration: none; margin: auto; float: initial" href="<?= HOME ?>login">Fazer Login</a>
-        </div>
-
     </div>
-</div>
 <?php
 $data['data'] = ob_get_contents();
 ob_end_clean();
--- public/view/404.php
+++ public/view/404.php
@@ -1,28 +1,31 @@
 <?php
 ob_start();
 ?>
-<div class="row">
-    <div class="panel align-center" style="max-width: 900px; margin: auto; float: initial">
-
+    <div class="r-404">
         <div class="row">
-            <div class="col s12 m6">
-                <br>
-                <div class="panel font-xlarge font-light padding-32">
-                    Algo deu errado...
+            <div class="panel align-center" style="max-width: 900px; margin: auto; float: initial">
+
+                <div class="row">
+                    <div class="col s12 m6">
+                        <br>
+                        <div class="panel font-xlarge font-light padding-32">
+                            Algo deu errado...
+                        </div>
+                    </div>
+                    <div class="col s12 m6">
+                        <h1 class="font-jumbo">404</h1>
+                    </div>
                 </div>
-            </div>
-            <div class="col s12 m6">
-                <h1 class="font-jumbo">404</h1>
+
+                <br><br>
+                <div class="align-center">
+                    <a class="btn-large opacity hover-shadow color-white"
+                       style="text-decoration: none; margin: auto; float: initial" href="<?= HOME ?>">Home</a>
+                </div>
+
             </div>
         </div>
-
-        <br><br>
-        <div class="align-center">
-            <a class="btn-large opacity hover-shadow color-white" style="text-decoration: none; margin: auto; float: initial" href="<?= HOME ?>">Home</a>
-        </div>
-
     </div>
-</div>
 <?php
 $data['data'] = ob_get_contents();
 ob_end_clean();

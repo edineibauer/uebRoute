17e6248749085c1c68a678424bc410ed297d6479
Edinei J. Bauer <galera.org@gmail.com>
2020-06-14 10:48:05 -0300

    fix response view

--- public/view/network/network.php
+++ public/view/network/network.php
@@ -1,38 +1,32 @@
-<?php
-ob_start();
-?>
-    <div class="r-network">
-        <div class="row">
-            <div class="panel align-center container-900">
+<div class="r-network">
+    <div class="row">
+        <div class="panel align-center container-900">
 
-                <div class="row padding-48 padding-bottom">
+            <div class="row padding-48 padding-bottom">
+                <div class="col s12">
+                    <br>
                     <div class="col s12">
-                        <br>
-                        <div class="col s12">
-                            <img src="<?= HOME ?>assetsPublic/img/nonetwork.svg?v=<?= VERSION ?>">
-                        </div>
-                        <div class="col panel font-large font-light padding-12">
-                            sem conexão
-                        </div>
-                        <p>
-                            volte quando estiver conectado
-                        </p>
+                        <img src="<?= HOME ?>assetsPublic/img/nonetwork.svg?v=<?= VERSION ?>">
                     </div>
+                    <div class="col panel font-large font-light padding-12">
+                        sem conexão
+                    </div>
+                    <p>
+                        volte quando estiver conectado
+                    </p>
                 </div>
-
-                <div class="align-center">
-                    <a class="btn opacity hover-shadow relative"
-                       style="text-decoration: none; margin: auto; float: initial; padding-left: 45px;border-radius: 5px;background: #eee;color: #222222;"
-                       href="<?= HOME ?>">
-                        <i class="material-icons padding-right"
-                           style="font-size: 20px;position: absolute;left: 15px;top: 6px;">home</i>
-                        <span class="upper">Página inicial</span>
-                    </a>
-                </div>
-
             </div>
+
+            <div class="align-center">
+                <a class="btn opacity hover-shadow relative"
+                   style="text-decoration: none; margin: auto; float: initial; padding-left: 45px;border-radius: 5px;background: #eee;color: #222222;"
+                   href="<?= HOME ?>">
+                    <i class="material-icons padding-right"
+                       style="font-size: 20px;position: absolute;left: 15px;top: 6px;">home</i>
+                    <span class="upper">Página inicial</span>
+                </a>
+            </div>
+
         </div>
     </div>
-<?php
-$data['data'] = ob_get_contents();
-ob_end_clean();
+</div>
